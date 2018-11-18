8d1bf09698c32ce1a85eed9638fe037051b63082
notjulie <rasmussenrandy189@gmail.com>
2018-11-18 11:19:00 -0600

    Sorting works.

--- DecisionTree/DecisionFeature.h
+++ DecisionTree/DecisionFeature.h
@@ -10,11 +10,11 @@ public:
    AbstractDecisionFeature(void);
 
    bool operator!=(const AbstractDecisionFeature &f) const {
-      throw DecisionTreeException("AbstractDecisionFeature::operator!=: not implemented");
+      return Compare(f) != 0;
    }
 
    bool operator<(const AbstractDecisionFeature &f) const {
-      throw DecisionTreeException("AbstractDecisionFeature::operator<: not implemented");
+      return Compare(f) < 0;
    }
 
    std::function<bool(const AbstractDecisionFeature &)>GetLessThanComparator(void) const {
@@ -23,6 +23,9 @@ public:
 
 public:
    static void RegisterConstructorNotification(const std::function<void(AbstractDecisionFeature *)> &hook);
+
+protected:
+   virtual int Compare(const AbstractDecisionFeature &f) const = 0;
 
 private:
    static thread_local std::function<void(AbstractDecisionFeature *)> constructorCallback;
@@ -38,6 +41,25 @@ public:
    }
 
    T operator*(void) { return value; }
+
+protected:
+   virtual int Compare(const AbstractDecisionFeature &_f) const {
+      // cast it to one of our own
+      auto f = dynamic_cast<const DecisionFeature *>(&_f);
+
+      // if it's not one of our own that probably has to be an error until I come up with a
+      // reason that it's not
+      if (f == nullptr)
+         throw DecisionTreeException("DecisionFeature::Compare: not same type");
+
+      // same type, it gets simple
+      if (value < f->value)
+         return -1;
+      else if (value > f->value)
+         return 1;
+      else
+         return 0;
+   }
 
 private:
    T  value;
